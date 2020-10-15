e0be8a3d1b2691db23a9209d1b3385919369a19b
hron2017 <andreypbiz@gmail.com>
2020-10-15 19:43:22 +0300

    comit

--- #source/js/main.js
+++ #source/js/main.js
@@ -3,7 +3,6 @@
 //@prepros-append blocks/mobile-menu.js
 //@prepros-append blocks/popup.js
 //@prepros-append blocks/spoller.js
-//@prepros-append blocks/dynamic_adapt.js
 
 //@prepros-append script.js
 
